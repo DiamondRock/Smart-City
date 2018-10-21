026c845024dc3a2d5e145c6cbc37bbd2c53dad1b
leodd <410528518@qq.com>
2018-10-20 21:00:51 -0500

    report app

---
+++ city-report/src/components/PhotoCapture.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="photo-capture">
+    <video muted autoplay ref="video"></video>
+    <div class="photo" @click="getImage"></div>
+  </div>
+</template>
+
+<script>
+/* eslint-disable */
+export default {
+  name: "PhotoCapture",
+
+  data() {
+    return {
+      cameraList: [],
+      selectedCameraIdx: 0,
+      image: null
+    };
+  },
+
+  computed: {
+    camera() {
+      return this.cameraList[this.selectedCameraIdx];
+    }
+  },
+
+  mounted() {
+    this.videoElement = this.$refs.video;
+    navigator.mediaDevices
+      .enumerateDevices()
+      .then(this.gotDevices)
+      .then(this.getStream)
+      .catch(this.handleError);
+  },
+
+  methods: {
+    gotDevices(infos) {
+      infos.forEach(info => {
+        if (info.kind === "videoinput") {
+          this.cameraList.push(info.deviceId);
+        }
+      });
+      this.selectedCameraIdx = this.cameraList.length - 1;
+    },
+
+    getStream() {
+      if (window.stream) {
+        window.stream.getTracks().forEach(track => {
+          track.stop();
+        });
+      }
+
+      navigator.mediaDevices
+        .getUserMedia({
+          video: {
+            deviceId: { exact: this.camera }
+          }
+        })
+        .then(this.gotStream)
+        .catch(this.handleError);
+    },
+
+    gotStream(stream) {
+      window.stream = stream;
+      this.videoElement.srcObject = stream;
+    },
+
+    handleError(error) {
+      console.log("Error: ", error);
+    },
+
+    getImage() {
+      let canvas = document.createElement('canvas')
+      let context = canvas.getContext('2d')
+      canvas.width = this.videoElement.clientWidth
+      canvas.height = this.videoElement.clientHeight
+      context.drawImage(this.videoElement, 0, 0, canvas.width, canvas.height)
+      this.image = canvas.toDataURL()
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.photo-capture {
+  video {
+    position: fixed;
+    right: 0;
+    bottom: 0;
+    min-width: 100%;
+    min-height: 100%;
+    background-color: #353535;
+  }
+
+  .photo {
+    position: fixed;
+    bottom: 3em;
+    left: 50%;
+    margin-left: -3em;
+    height: 6em;
+    width: 6em;
+    border-radius: 6em;
+    border: solid 0.5em #fff;
+    background-color: #7b50ff;
+    cursor: pointer;
+  }
+}
+</style>
---
+++ city-report/src/components/ReportTypeSelection.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="report-type-selection">
+    <p class="prompt">PLEASE SELECT YOUR REPORT TYPE</p>
+    <div class="item"
+         v-for="item in type"
+         :key="item.typeId"
+         @click="selectedId = item.typeId"
+         :style="{backgroundColor: selectedId === item.typeId ? selectedColor : unselectedColor}">
+      <!--<img :src="'@/assets/' + item.imgId + '.png'">-->
+      <p class="name">{{ item.name }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ReportTypeSelection",
+
+  data() {
+    return {
+      type: [
+        { typeId: 1, name: "Fire", imgId: "img-fire" },
+        { typeId: 2, name: "Car Accident", imgId: "img-car-accident" },
+        { typeId: 3, name: "Broken Facility", imgId: "img-broken-facility" }
+      ],
+      selectedId: 0,
+      selectedColor: "#7b50ff",
+      unselectedColor: "#353535"
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+.report-type-selection {
+  .prompt {
+    margin: 40px 0;
+    font-size: 1.2em;
+  }
+
+  .item {
+    height: 100px;
+    width: auto;
+    margin: 10px;
+    line-height: 100px;
+    border-radius: 5px;
+    cursor: pointer;
+
+    .name {
+      margin: 0;
+      color: #fff;
+      font-size: 1.8em;
+    }
+  }
+}
+</style>
